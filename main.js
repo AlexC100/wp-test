2d8fc986a5aa21d2baa6fc7483926bb6ce66ef27
Alexandru Ceausu <alexandru.ceausu87@gmail.com>
2021-03-27 18:31:43 +0200

    jhgkjhgasd

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ axios
 
     if (window.location.pathname === "/changed.html") {
       UIChangeText.textContent = "Title Changed";
+    } else if (window.location.pathname === "/page2.html") {
+      UIChangeText.textContent = "This is page 2";
+    } else if (window.location.pathname === "/index.html") {
+      UIChangeText.textContent = "This is the index page";
     }
   })
   .catch((err) => console.log(err));
